43b2e8f1bb74ad804ca69b63b4ec62e6a96a75f3
analytics@thenursecoaches.com <analytics@thenursecoaches.com>
2022-04-21 22:05:52 +0000

    asf

--- target/compiled/tnc_data_pipeline/models/admin/accounts_proc.sql
+++ target/compiled/tnc_data_pipeline/models/admin/accounts_proc.sql
@@ -6,12 +6,12 @@ platform
 from  (
  
 	select  
-	store as site,
+	site,
 	bigquery_name,
 	account,
 	platform,
 	time_of_entry,
-	first_value(time_of_entry) over (partition by store order by time_of_entry desc) lv
+	first_value(time_of_entry) over (partition by site order by time_of_entry desc) lv
 	from `tnc-data-pipeline-347720.thenursecoaches_data_pipeline.accounts`
 ) 
 
