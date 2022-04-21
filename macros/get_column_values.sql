07a0a5dc3f0d5289bfbfab94ad0d641bba1f737f
tom <101282556+smarterdata-agency@users.noreply.github.com>
2022-04-21 14:39:19 -0700

    initial commit

---
+++ macros/get_column_values.sql
@@ -0,0 +1,47 @@
+{% macro get_column_values(table, column, max_records=none, filter_column=none, filter_value=none, filter_column_2=none, filter_value_2=none, filter_column_3=none, filter_value_3=none, group=True) -%}
+
+    {%- call statement('get_column_values', fetch_result=True) %}
+
+        select
+            {{ column }} as value
+
+        from {{ table }}
+        where {{ column }} is not null
+        and {{ column }} != ''
+
+        {% if filter_column is not none %}
+        ##where 1 = 1
+        and {{ filter_column }} = '{{ filter_value }}'
+        {% endif %}
+        
+        {% if filter_column_2 is not none %}
+        ##and 2 = 2
+        and {{ filter_column_2 }} = '{{ filter_value_2 }}'
+        {% endif %}
+
+        {% if filter_column_3 is not none %}
+        ##and 3 = 3
+        and {{ filter_column_3 }} = '{{ filter_value_3 }}'
+        {% endif %}
+
+        {% if group %}
+        group by 1
+        order by count(*) desc
+        {% endif %}
+        
+        {% if max_records is not none %}
+        limit {{ max_records }}
+        {% endif %}
+
+    {%- endcall -%}
+
+    {%- set value_list = load_result('get_column_values') -%}
+
+    {%- if value_list and value_list['data'] -%}
+        {%- set values = value_list['data'] | map(attribute=0) | list %}
+        {{ return(values) }}
+    {%- else -%}
+        {{ return([]) }}
+    {%- endif -%}
+
+{%- endmacro %}
