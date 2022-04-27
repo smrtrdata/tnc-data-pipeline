3f7b6562b754b0e46edb3d531033f7f86d1ec197
analytics@thenursecoaches.com <analytics@thenursecoaches.com>
2022-04-27 20:30:11 +0000

    asdf

--- target/compiled/tnc_data_pipeline/models/adwords/google_ads_campaigns.sql
+++ target/compiled/tnc_data_pipeline/models/adwords/google_ads_campaigns.sql
@@ -28,7 +28,7 @@ FROM
 	,impressions
 	,clicks
 	,_sdc_sequence
-	,first_value(_sdc_sequence) OVER (PARTITION BY campaignid, date ORDER BY _sdc_sequence DESC) lv
+	,first_value(_sdc_sequence) OVER (PARTITION BY campaign_id, date ORDER BY _sdc_sequence DESC) lv
 	FROM `tnc-data-pipeline-347720.google_ads_thenursecoaches.campaign_performance_report`
 
 
