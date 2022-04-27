3634726ec39467210bf82a0ea70164f1b85ae557
analytics@thenursecoaches.com <analytics@thenursecoaches.com>
2022-04-27 17:56:14 +0000

    asdf

--- target/run/tnc_data_pipeline/models/facebook/fb_ads_insights.sql
+++ target/run/tnc_data_pipeline/models/facebook/fb_ads_insights.sql
@@ -13,20 +13,18 @@ WITH fb_ads_insights as (
 
 	    
 		   	SELECT 
-		   	account_name,
-			date_start,
-			campaign_id,
-			campaign_name,
-			adset_id,
-			adset_name,
-			ad_id,
-			ad_name,
-			spend,
-			cpm,
-			inline_link_click_ctr,
-			reach,
-			impressions,
-			inline_link_clicks,
+		   	account_name
+			,date_start,
+			,campaign_id
+			,campaign_name
+			,adset_id
+			,adset_name
+			,ad_id
+			,ad_name
+			,spend
+			,inline_link_click_ctr
+			,impressions
+			,inline_link_clicks
 			##_sdc_sequence,
 			##first_value(_sdc_sequence) OVER (PARTITION BY date_start, ad_id, campaign_id ORDER BY _sdc_sequence DESC) lv
 			FROM `tnc-data-pipeline-347720.fb_thenursecoaches.ads_insights`
@@ -36,33 +34,28 @@ WITH fb_ads_insights as (
 )
 
 SELECT
-account_name account,
-date_start date,
-campaign_id,
-campaign_name campaign,
-adset_id,
-adset_name,
-ad_id,
-ad_name,
-sum(spend) cost,
-avg(cpm) cpm,
-sum(reach) reach,
-sum(impressions) impressions,
-sum(inline_link_clicks) clicks,
-safe_divide(sum(spend),sum(inline_link_clicks)) cpc,
-safe_divide(sum(inline_link_clicks),sum(reach)) ctr
-
+account_name account
+,date_start date
+,campaign_id
+,campaign_name campaign
+,adset_id
+,adset_name
+,ad_id
+,ad_name
+,sum(spend) fb_cost
+,sum(impressions) fb_impressions
+,sum(inline_link_clicks) fb_clicks
 FROM fb_ads_insights
 ##where lv = _sdc_sequence
 GROUP BY 
-account,
-date,
-campaign_id,
-campaign,
-adset_id,
-adset_name,
-ad_id,
-ad_name
+account
+date
+campaign_id
+,campaign
+,adset_id
+,adset_name
+,ad_id
+,ad_name
 
 
   );
