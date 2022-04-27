d002e96fa0a58dd53692e8a23104c5d139009281
analytics@thenursecoaches.com <analytics@thenursecoaches.com>
2022-04-27 22:00:20 +0000

    asdf

--- target/run/tnc_data_pipeline/models/facebook/fb_ads_campaigns.sql
+++ target/run/tnc_data_pipeline/models/facebook/fb_ads_campaigns.sql
@@ -9,13 +9,13 @@
 account
 ,date
 ,campaign_id
-,campaign_name campaign
+,campaign
 ,campaign_advertising_channel_type
 ,channel
 ,platform
-,sum(spend) cost
+,sum(cost) cost
 ,sum(impressions) impressions
-,sum(inline_link_clicks) clicks
+,sum(clicks) clicks
 FROM `tnc-data-pipeline-347720`.`development`.`fb_ads_insights`
 
 ##where lv = _sdc_sequence
