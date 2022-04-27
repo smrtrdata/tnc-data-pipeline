6f51fb890fc554e5864c4233422b07066b77a83f
tom <101282556+smarterdata-agency@users.noreply.github.com>
2022-04-27 13:01:25 -0700

    Create google_ads_campaigns.sql

---
+++ models/adwords/google_ads_campaigns.sql
@@ -0,0 +1,33 @@
+{% set accounts = get_column_values(table=ref('accounts_proc'), column='account', max_records=50, filter_column='platform', filter_value='Google Ads') %}
+
+{% if accounts != [] %}
+
+
+SELECT
+date
+,campaignid
+,campaign_name
+,campaign_advertising_channel_type
+,channel
+,platform
+,sum(cost) cost
+,sum(impressions) impressions
+,sum(clicks) clicks
+,sum(conversions) conversions
+FROM
+( 
+	SELECT  
+	'Paid' as channel
+	,'Adwords' as platform
+	,date
+	,campaignid
+	,campaign_name
+	,cost_micros/1000000 cost,
+	,impressions,
+	,clicks,
+	,_sdc_sequence
+	,first_value(_sdc_sequence) OVER (PARTITION BY campaignid, day ORDER BY _sdc_sequence DESC) lv
+	FROM `{{ target.project }}.adwords.CAMPAIGN_PERFORMANCE_REPORT`
+)
+WHERE lv = _sdc_sequence
+GROUP BY date, campaignid, account, channel, platform, campaign
