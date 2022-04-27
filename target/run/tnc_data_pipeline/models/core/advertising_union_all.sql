cf5f0410f8c4e97d6e61d3868b90804434fd029b
analytics@thenursecoaches.com <analytics@thenursecoaches.com>
2022-04-27 22:48:06 +0000

    asdf

--- target/run/tnc_data_pipeline/models/core/advertising_union_all.sql
+++ target/run/tnc_data_pipeline/models/core/advertising_union_all.sql
@@ -16,6 +16,12 @@ account
 ,cost
 ,impressions
 ,clicks
+,cost as fb_cost
+,impressions as fb_impressions
+,clicks as fb_clicks
+,0 as google_ads_cost
+,0 as google_ads_impressions
+,0 as google_ads_clicks
 FROM `tnc-data-pipeline-347720`.`development`.`fb_ads_campaigns`
 
 
@@ -35,6 +41,12 @@ account
 ,cost
 ,impressions
 ,clicks
+,cost as google_ads_cost
+,impressions as google_ads_impressions
+,clicks as google_ads_clicks
+,0 as fb_cost
+,0 as fb_impressions
+,0 as fb_clicks
 FROM `tnc-data-pipeline-347720`.`development`.`google_ads_campaigns`
   );
   
